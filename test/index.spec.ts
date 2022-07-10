7802ef020f8493b1c46bcab9d9587cdecdc22bb5
Krzysztof Rudowski <krzysztof.rudowski@schibsted.pl>
2022-07-11 01:00:20 +0200

    fix: improve tests

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -4,34 +4,6 @@ import yupToOpenAPI from '@src/index';
 
 describe('yup-to-openapi', () => {
     describe('yup mixed schema', () => {
-        it('should not handle "required" attribute', () => {
-            const input = yup.string().required();
-            const output: SchemaObject = { type: 'string' };
-
-            expect(yupToOpenAPI(input)).toEqual(output);
-        });
-
-        it('should handle "nullable" attribute', () => {
-            const input = yup.string().nullable();
-            const output: SchemaObject = { type: 'string', nullable: true };
-
-            expect(yupToOpenAPI(input)).toEqual(output);
-        });
-
-        it('should handle "max" attribute', () => {
-            const input = yup.string().default('default value');
-            const output: SchemaObject = { type: 'string', default: 'default value' };
-
-            expect(yupToOpenAPI(input)).toEqual(output);
-        });
-
-        it('should handle "enum" attribute', () => {
-            const input = yup.string().oneOf(['a', 'b', 'c']);
-            const output: SchemaObject = { type: 'string', enum: ['a', 'b', 'c'] };
-
-            expect(yupToOpenAPI(input)).toEqual(output);
-        });
-
         it('should handle "label" attribute', () => {
             const input = yup.string().label('label');
             const output: SchemaObject = { type: 'string', title: 'label' };
@@ -46,16 +18,137 @@ describe('yup-to-openapi', () => {
             expect(yupToOpenAPI(input)).toEqual(output);
         });
 
-        it('should use title in "meta" attribute instead of label', () => {
-            const input = yup.string().label('label').meta({ title: 'title' });
-            const output: SchemaObject = { type: 'string', title: 'title' };
+        it('should handle description in "meta" attribute', () => {
+            const input = yup.string().meta({ description: 'description' });
+            const output: SchemaObject = { type: 'string', description: 'description' };
 
             expect(yupToOpenAPI(input)).toEqual(output);
         });
 
-        it('should handle description in "meta" attribute', () => {
-            const input = yup.string().meta({ description: 'description' });
-            const output: SchemaObject = { type: 'string', description: 'description' };
+        it('should handle "nullable" attribute', () => {
+            const input = yup.string().nullable();
+            const output: SchemaObject = { type: 'string', nullable: true };
+
+            expect(yupToOpenAPI(input)).toEqual(output);
+        });
+
+        describe('should handle "default" attribute', () => {
+            it('that is string', () => {
+                const input = yup.string().default('default value');
+                const output: SchemaObject = { type: 'string', default: 'default value' };
+
+                expect(yupToOpenAPI(input)).toEqual(output);
+            });
+
+            it('that is number', () => {
+                const input = yup.number().default(1.23);
+                const output: SchemaObject = { type: 'number', format: 'float', default: 1.23 };
+
+                expect(yupToOpenAPI(input)).toEqual(output);
+            });
+
+            it('that is integer', () => {
+                const input = yup.number().integer().default(100);
+                const output: SchemaObject = { type: 'integer', format: 'int32', default: 100 };
+
+                expect(yupToOpenAPI(input)).toEqual(output);
+            });
+
+            it('that is boolean', () => {
+                const input = yup.boolean().default(true);
+                const output: SchemaObject = { type: 'boolean', default: true };
+
+                expect(yupToOpenAPI(input)).toEqual(output);
+            });
+
+            it('that is function returning Date object', () => {
+                const input = yup.date().default(function now() {
+                    return new Date('2021-01-01T00:00:00.000Z');
+                });
+                const output: SchemaObject = {
+                    type: 'string',
+                    format: 'date',
+                    default: '2021-01-01T00:00:00.000Z',
+                };
+
+                expect(yupToOpenAPI(input)).toEqual(output);
+            });
+
+            it('that is Date object', () => {
+                const input = yup.date().default(new Date('2021-01-01T00:00:00.000Z'));
+                const output: SchemaObject = {
+                    type: 'string',
+                    format: 'date',
+                    default: '2021-01-01T00:00:00.000Z',
+                };
+
+                expect(yupToOpenAPI(input)).toEqual(output);
+            });
+        });
+
+        it('should not handle "default" attribute for array', () => {
+            const input = yup.array().default(['a', 'b', 'c']);
+            const output: SchemaObject = { type: 'array' };
+
+            expect(yupToOpenAPI(input)).toEqual(output);
+        });
+
+        it('should not handle "default" attribute for object', () => {
+            const input = yup
+                .object({
+                    foo: yup.string(),
+                    bar: yup.string(),
+                })
+                .default({ foo: 'foo', bar: 'bar' });
+            const output: SchemaObject = {
+                type: 'object',
+                properties: {
+                    foo: { type: 'string' },
+                    bar: { type: 'string' },
+                },
+            };
+
+            expect(yupToOpenAPI(input)).toEqual(output);
+        });
+
+        describe('should handle "oneOf" attribute', () => {
+            it('that is array of strings', () => {
+                const input = yup.string().oneOf(['a', 'b', 'c']);
+                const output: SchemaObject = { type: 'string', enum: ['a', 'b', 'c'] };
+
+                expect(yupToOpenAPI(input)).toEqual(output);
+            });
+
+            it('that is array of numbers', () => {
+                const input = yup.number().oneOf([1, 2, 3]);
+                const output: SchemaObject = { type: 'number', format: 'float', enum: [1, 2, 3] };
+
+                expect(yupToOpenAPI(input)).toEqual(output);
+            });
+        });
+
+        it('should handle "required" attribute', () => {
+            const input = yup.object({
+                foo: yup.string().required(),
+                bar: yup.string(),
+                baz: yup.number().required(),
+            });
+            const output: SchemaObject = {
+                type: 'object',
+                properties: {
+                    foo: { type: 'string' },
+                    bar: { type: 'string' },
+                    baz: { type: 'number', format: 'float' },
+                },
+                required: ['foo', 'baz'],
+            };
+
+            expect(yupToOpenAPI(input)).toEqual(output);
+        });
+
+        it('should not handle "required" attribute for string that is not part of object schema', () => {
+            const input = yup.string().required();
+            const output: SchemaObject = { type: 'string' };
 
             expect(yupToOpenAPI(input)).toEqual(output);
         });
@@ -79,6 +172,13 @@ describe('yup-to-openapi', () => {
         it('should handle "max" attribute', () => {
             const input = yup.string().max(10);
             const output: SchemaObject = { type: 'string', maxLength: 10 };
+
+            expect(yupToOpenAPI(input)).toEqual(output);
+        });
+
+        it('should handle "matches" attribute', () => {
+            const input = yup.string().matches(/(hi|bye)/);
+            const output: SchemaObject = { type: 'string', pattern: '/(hi|bye)/' };
 
             expect(yupToOpenAPI(input)).toEqual(output);
         });
@@ -128,36 +228,36 @@ describe('yup-to-openapi', () => {
         });
 
         it('should handle "max" attribute', () => {
-            const input = yup.number().max(1);
-            const output: SchemaObject = { type: 'number', format: 'float', maximum: 1 };
+            const input = yup.number().max(10);
+            const output: SchemaObject = { type: 'number', format: 'float', maximum: 10 };
 
             expect(yupToOpenAPI(input)).toEqual(output);
         });
 
         it('should handle "positive" attribute', () => {
             const input = yup.number().positive();
-            const output: SchemaObject = { type: 'number', format: 'float', minimum: 0 };
+            const output: SchemaObject = { type: 'number', format: 'float', minimum: 0, exclusiveMinimum: true };
 
             expect(yupToOpenAPI(input)).toEqual(output);
         });
 
         it('should handle "negative" attribute', () => {
             const input = yup.number().negative();
-            const output: SchemaObject = { type: 'number', format: 'float', maximum: 0 };
-
-            expect(yupToOpenAPI(input)).toEqual(output);
-        });
-
-        it('should handle "moreThan" attribute', () => {
-            const input = yup.number().moreThan(10);
-            const output: SchemaObject = { type: 'number', format: 'float', minimum: 10 };
+            const output: SchemaObject = { type: 'number', format: 'float', maximum: 0, exclusiveMaximum: true };
 
             expect(yupToOpenAPI(input)).toEqual(output);
         });
 
         it('should handle "lessThan" attribute', () => {
-            const input = yup.number().lessThan(100);
-            const output: SchemaObject = { type: 'number', format: 'float', maximum: 100 };
+            const input = yup.number().lessThan(10);
+            const output: SchemaObject = { type: 'number', format: 'float', maximum: 10, exclusiveMaximum: true };
+
+            expect(yupToOpenAPI(input)).toEqual(output);
+        });
+
+        it('should handle "moreThan" attribute', () => {
+            const input = yup.number().moreThan(1);
+            const output: SchemaObject = { type: 'number', format: 'float', minimum: 1, exclusiveMinimum: true };
 
             expect(yupToOpenAPI(input)).toEqual(output);
         });
@@ -174,13 +274,10 @@ describe('yup-to-openapi', () => {
 
     describe('yup date schema', () => {
         it('should convert schema to openapi schema', () => {
-            const input = yup.date().default(function now() {
-                return new Date('2021-01-01T00:00:00.000Z');
-            });
+            const input = yup.date();
             const output: SchemaObject = {
                 type: 'string',
                 format: 'date',
-                default: new Date('2021-01-01T00:00:00.000Z'),
             };
 
             expect(yupToOpenAPI(input)).toEqual(output);
@@ -199,37 +296,40 @@ describe('yup-to-openapi', () => {
 
             expect(yupToOpenAPI(input)).toEqual(output);
         });
-    });
 
-    describe('yup object schema', () => {
-        it('should convert schema to openapi schema', () => {
-            const input = yup.object({
-                prop1: yup.string(),
-                prop2: yup.string(),
-            });
+        it('should handle "min" attribute', () => {
+            const input = yup.array().min(1);
             const output: SchemaObject = {
-                type: 'object',
-                properties: {
-                    prop1: { type: 'string' },
-                    prop2: { type: 'string' },
-                },
+                type: 'array',
+                minItems: 1,
             };
 
             expect(yupToOpenAPI(input)).toEqual(output);
         });
 
-        it('should handle "required" attribute', () => {
+        it('should handle "max" attribute', () => {
+            const input = yup.array().max(1);
+            const output: SchemaObject = {
+                type: 'array',
+                maxItems: 1,
+            };
+
+            expect(yupToOpenAPI(input)).toEqual(output);
+        });
+    });
+
+    describe('yup object schema', () => {
+        it('should convert schema to openapi schema', () => {
             const input = yup.object({
-                prop1: yup.string().required(),
-                prop2: yup.string(),
+                foo: yup.string(),
+                bar: yup.string(),
             });
             const output: SchemaObject = {
                 type: 'object',
                 properties: {
-                    prop1: { type: 'string' },
-                    prop2: { type: 'string' },
+                    foo: { type: 'string' },
+                    bar: { type: 'string' },
                 },
-                required: ['prop1'],
             };
 
             expect(yupToOpenAPI(input)).toEqual(output);
